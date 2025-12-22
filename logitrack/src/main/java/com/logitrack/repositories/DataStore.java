eebf820b861f68f688a5d29e25ba436801221c5e
Douglas Cetino <2473138152001@ingenieria.usac.edu.gt>
2025-12-22 12:18:20 -0600

    repositorio en memoria

---
+++ logitrack/src/main/java/com/logitrack/repositories/DataStore.java
@@ -0,0 +1,30 @@
+package com.logitrack.repositories;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+import com.logitrack.models.Centro;
+import com.logitrack.models.Mensajero;
+import com.logitrack.models.Paquete;
+import com.logitrack.models.Ruta;
+import com.logitrack.models.Solicitud;
+
+public class DataStore {
+
+    public static Map<String, Centro> centros = new HashMap<>();
+    public static Map<String, Ruta> rutas = new HashMap<>();
+    public static Map<String, Mensajero> mensajeros = new HashMap<>();
+    public static Map<String, Paquete> paquetes = new HashMap<>();
+
+    public static PriorityQueue<Solicitud> solicitudes =
+            new PriorityQueue<>((a, b) -> b.getPrioridad() - a.getPrioridad());
+
+    public static void limpiar() {
+        centros.clear();
+        rutas.clear();
+        mensajeros.clear();
+        paquetes.clear();
+        solicitudes.clear();
+    }
+}
